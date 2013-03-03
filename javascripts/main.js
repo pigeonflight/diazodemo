b439612b6a344bf056e09ecf9b4800fff6c189e5
David Bain <david@alteroo.com>
2013-03-03 07:25:45 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 console.log('This would be the main JS file.');
 $(document).ready(function(){
   
-   $('#theme_panel').load('../../../@@theming-controlpanel #fieldset-advanced');
+   $('#theme_panel .inner').load('../../../@@theming-controlpanel #fieldset-advanced');
   
   });
