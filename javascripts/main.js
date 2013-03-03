fae9afc15ff4e09cac38dcdc3a3ae5204e204dc4
David Bain <david@alteroo.com>
2013-03-03 08:01:22 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,15 @@ $(document).ready(function(){
        $('#theme_panel .inner').load('../../../@@theming-controlpanel #fieldset-advanced',function() {
           
           $('#theme_panel button').on('click', function(){
-               $('#theme_panel fieldset')[1].style.display="none";
+               var fields = $('#theme_panel fieldset')[0].querySelectorAll('.field')
+               var field_length = fields.length;
+               
+               for (var i=1;i<fields.length;i++)
+                { 
+                    fields[i].remove();
+                 }
+                 
+               $('#theme_panel fieldset')[1].remove();
                $('#theme_panel .inner').fadeToggle();
                
           });
