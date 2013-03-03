ba6dc8531a19cbd10326fbef27100f28d321bf00
David Bain <david@alteroo.com>
2013-03-03 07:48:18 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
        $('#theme_panel .inner').load('../../../@@theming-controlpanel #fieldset-advanced',function() {
           
           $('#theme_panel button').on('click', function(){
-               
+               $('#theme_panel fieldset')[1].style.display="none";
                $('#theme_panel .inner').fadeToggle();
                
           });
