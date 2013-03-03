445a46811e2a10ae2a4b8db50cb83a90933b1f1b
David Bain <david@alteroo.com>
2013-03-03 07:35:12 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,15 @@
 console.log('This would be the main JS file.');
 $(document).ready(function(){
   
-   $('#theme_panel .inner').load('../../../@@theming-controlpanel #fieldset-advanced');
+       $('#theme_panel .inner').load('../../../@@theming-controlpanel #fieldset-advanced',function() {
+
+          $('#theme_panel button').on('click', function(){
+               
+               $('#theme_panel .inner').fadeToggle();
+               
+          });
   
+     
+       });
+     
   });
